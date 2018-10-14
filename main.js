f0bfe1c476ebec546011e8635d90551b8950cc31
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-14 16:12:53 -0700

    removed code template

--- main.js
+++
@@ -1,9 +0,0 @@
-for (var i = 0; i < allImages.length; i++) {
-
-    var imageHeight = allImages[i].clientHeight;
-    var imageWidth = allImages[i].clientWidth;
-
-    allImages[i].setAttribute('src',
-        'http://placedog.net/' + imageWidth + '/' + imageHeight);
-
-}
