4e75434e9255789c58fb7e4472e41f1d5e8d4976
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-15 20:22:06 -0700

    airports change

--- main.js
+++ main.js
@@ -73225,7 +73225,19 @@ const airports = [{
 ];
 
 for (var j = 0; j < document.getElementsByClassName('map-marker-label').length; j++) {
-  code = [];
-  city = document.getElementsByClassName('map-marker-label')[j].innerHTML;
-  
-  }}
+    code = [];
+    city = document.getElementsByClassName('map-marker-label')[j].innerHTML;
+    for (var i = 0; i < airports.length; i++) {
+        if (airports[i].name) {
+            bool = airports[i].name.includes(city);
+            if (bool) {
+                code.push(airports[i].iata)
+            }
+        }
+        if (code === []) {
+            document.getElementsByClassName('map-marker-label')[j].innerHTML = city;
+        } else {
+            document.getElementsByClassName('map-marker-label')[j].innerHTML = code.join(", ");
+        }
+    }
+}
