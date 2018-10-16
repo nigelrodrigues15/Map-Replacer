f781d6c26fe2d5d3ff95776d8f1b4ebb696d9f6f
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-15 19:48:24 -0700

    remove comments

--- main.js
+++ main.js
@@ -15,11 +15,11 @@ const allImages = document.getElementsByTagName('img');
 
 for (var i = 0; i < allImages.length; i++) {
 
-  var imageHeight = allImages[i].clientHeight;
-  var imageWidth = allImages[i].clientWidth;
+    var imageHeight = allImages[i].clientHeight;
+    var imageWidth = allImages[i].clientWidth;
 
-  allImages[i].setAttribute('src',
-  'http://placedog.net/' + imageWidth + '/' + imageHeight );
+    allImages[i].setAttribute('src',
+        'http://placedog.net/' + imageWidth + '/' + imageHeight);
 
 }
 
@@ -49,14 +49,4 @@ for (var i = 0; i < allImages.length; i++) {
 //     this.setAttribute('style', 'filter: blur(5px)')
 
 //   })
-// }
-
-// document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
-//
-// for (var i = 0; i < allImages.length; i++) {
-//   var imageHeight = allImages[i].clientHeight;
-//   var imageWidth = allImages[i].clientWidth;
-//
-//   //cant use file for live links, only works for offline links
-//   allImages[i].setAttribute('src', 'file:///C:/Users/nigel/Documents/Coding_Directory/Canada%20Learning%20to%20Code/llc-intro-to-browser-extensions-master/first-extension/images/get_started128.png' );
 // }
