74c01df1898872e33b61f9d3eb51731e3ec51b37
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-15 19:49:11 -0700

    remove comments

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// //changes background color to purple
-// document.body.style.backgroundColor = "purple";
-
 // //changes css style of a the page
 // document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
 
