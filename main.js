ed581757c857254e7a3a3f607a1c88e6db3efb57
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-15 19:49:23 -0700

    remove comments

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-// //changes css style of a the page
-// document.body.style = "transition: 5s; transform: rotate(5deg) scale(0.8); background-color: grey; color: white;";
-
 // // get all html nodes with id="main" on the webpage and console log it
 // const allIds = document.getElementById("main");
 // console.log(allIds);
