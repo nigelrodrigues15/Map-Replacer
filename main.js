869d1643491f0afe9201beb3d35a20639679ae34
Nigel Rodrigues <nigel1@ualberta.ca>
2018-10-16 20:17:21 -0700

    import issues

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-    for (var j = 0; j < document.getElementsByClassName('map-marker-label').length; j++) {
-        code = [];
-        city = document.getElementsByClassName('map-marker-label')[j].innerHTML;
-        for (var i = 0; i < airports.length; i++) {
-            if (airports[i].name) {
-                bool = airports[i].name.includes(city);
-                if (bool) {
-                    code.push(airports[i].iata)
-                }
-            }
-            if (code === []) {
-                document.getElementsByClassName('map-marker-label')[j].innerHTML = city;
-            } else {
-                document.getElementsByClassName('map-marker-label')[j].innerHTML = code.join(", ");
+for (var j = 0; j < document.getElementsByClassName('map-marker-label').length; j++) {
+    code = [];
+    city = document.getElementsByClassName('map-marker-label')[j].innerHTML;
+    for (var i = 0; i < airports.length; i++) {
+        if (airports[i].name) {
+            bool = airports[i].name.includes(city);
+            if (bool) {
+                code.push(airports[i].iata)
             }
         }
+        if (code === []) {
+            document.getElementsByClassName('map-marker-label')[j].innerHTML = city;
+        } else {
+            document.getElementsByClassName('map-marker-label')[j].innerHTML = city + '('
+            code.join(", ") + ')';
+        }
     }
+}
